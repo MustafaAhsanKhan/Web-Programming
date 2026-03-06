38b75195ca319fe7b7c9e84fb87f5deef3c1ba07
MustafaAhsanKhan <mustafaahsankhan2006@gmail.com>
2026-03-06 15:46:20 +0500

    Added MongoDB connection using Mongoose

--- Task 3/server.js
+++ Task 3/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const connectDB = require('./db');
 
 const app = express();
 const PORT = 3000;
+
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
